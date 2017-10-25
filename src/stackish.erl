cdfa57ebbb2fbe0459fbad7f5ae7a12b38fa2549
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-10-25 10:33:06 +0800

    implemented parser for blob

--- src/stackish.erl
+++ src/stackish.erl
@@ -47,13 +47,27 @@ parse(<<" ", T/binary>>, Stack, Acc, normal) ->
 parse(<<"\"", T/binary>>, Stack, nil, normal) ->
   parse(T, Stack, "", string);
 parse(<<"\"", T/binary>>, Stack, Acc, string) ->
-  io:format("finishing string, before=~w~n", [Acc]),
-  io:format("after~w~n", [lists:reverse(Acc)]),
-  parse(T, [lists:reverse(Acc) | Stack], nil, string);
+  io:format("finishing string, before=~p~n", [Acc]),
+  Str = lists:reverse(Acc),
+  io:format("after=~p~n", [Str]),
+  parse(T, [Str | Stack], nil, string);
 parse(<<H, T/binary>>, Stack, Acc, string) ->
   parse(T, Stack, [H | Acc], string);
 
 %% parse blob
+parse(<<"'", T/binary>>, Stack, nil, normal) ->
+  parse(T, Stack, 0, blob_num);
+parse(<<H, T/binary>>, Stack, Len, blob_num) when ?is_digit(H) ->
+  parse(T, Stack, Len * 10 + (H - 48), blob_num);
+parse(<<":", T/binary>>, Stack, Len, blob_num) ->
+  Tuple = {Len, []},
+  parse(T, Stack, Tuple, blob);
+parse(<<H, T/binary>>, Stack, {Len, Acc}, blob) when is_integer(Len) andalso Len > 0 ->
+  Tuple = {Len - 1, [H | Acc]},
+  parse(T, Stack, Tuple, blob);
+parse(<<"'", T/binary>>, Stack, {0, Acc}, blob) ->
+  Blob = erlang:list_to_binary(lists:reverse(Acc)),
+  parse(T, [Blob | Stack], nil, normal);
 
 %% parse word
 
