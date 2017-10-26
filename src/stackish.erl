549fc4e4d2743dee33b3271c6402f72b89c1e7f8
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-10-26 15:48:27 +0800

    renamed parse() into tokenize()

--- src/stackish.erl
+++ src/stackish.erl
@@ -2,17 +2,19 @@
 
 -export([decode/1, encode/1]).
 
--compile(export_all).
+-record(node, {name, children}).
 
+-compile(export_all).
 
 %%%%%%%%%
 %% API %%
 %%%%%%%%%
 
 decode(Data) when is_binary(Data) ->
-  {ok, Stack} = parse(Data),
+  {ok, Stack} = tokenize(Data),
   io:format("Stack=~p~n", [Stack]),
-  {error, {not_impl, [Stack]}}.
+  Res = parse(Stack),
+  {error, {not_impl, [Res]}}.
 
 encode(_Data) -> not_impl.
 
@@ -31,74 +33,74 @@ encode(_Data) -> not_impl.
   (65 =< B andalso B =< (64 + 26)) or ((65 + 32) =< B andalso B =< (64 + 26 + 32))
 ).
 
-%%%%%%%%%%
-% parser %
-%%%%%%%%%%
+%%%%%%%%%%%%%
+% tokenizer %
+%%%%%%%%%%%%%
 
-parse(Data) ->
-  parse(Data, [], nil, normal).
+tokenize(Data) ->
+  tokenize(Data, [], nil, normal).
 
 % rules: https://zedshaw.com/archive/stackish-an-xml-alternative/
 
-parse(<<"[", T/binary>>, Stack, Acc, normal) ->
-  parse(T, [mark | Stack], Acc, normal);
+tokenize(<<"[", T/binary>>, Stack, Acc, normal) ->
+  tokenize(T, [mark | Stack], Acc, normal);
 
-parse(<<" ", T/binary>>, Stack, Acc, normal) ->
-  parse(T, Stack, Acc, normal);
+tokenize(<<" ", T/binary>>, Stack, Acc, normal) ->
+  tokenize(T, Stack, Acc, normal);
 
 %% parse string
-parse(<<"\"", T/binary>>, Stack, nil, normal) ->
-  parse(T, Stack, "", string);
-parse(<<"\"", T/binary>>, Stack, Acc, string) ->
+tokenize(<<"\"", T/binary>>, Stack, nil, normal) ->
+  tokenize(T, Stack, "", string);
+tokenize(<<"\"", T/binary>>, Stack, Acc, string) ->
   io:format("finishing string, before=~p~n", [Acc]),
   Str = lists:reverse(Acc),
   io:format("after=~p~n", [Str]),
-  parse(T, [Str | Stack], nil, normal);
-parse(<<H, T/binary>>, Stack, Acc, string) ->
-  parse(T, Stack, [H | Acc], string);
+  tokenize(T, [Str | Stack], nil, normal);
+tokenize(<<H, T/binary>>, Stack, Acc, string) ->
+  tokenize(T, Stack, [H | Acc], string);
 
 %% parse blob
-parse(<<"'", T/binary>>, Stack, nil, normal) ->
-  parse(T, Stack, 0, blob_num);
-parse(<<H, T/binary>>, Stack, Len, blob_num) when ?is_digit(H) ->
-  parse(T, Stack, Len * 10 + (H - 48), blob_num);
-parse(<<":", T/binary>>, Stack, Len, blob_num) ->
+tokenize(<<"'", T/binary>>, Stack, nil, normal) ->
+  tokenize(T, Stack, 0, blob_num);
+tokenize(<<H, T/binary>>, Stack, Len, blob_num) when ?is_digit(H) ->
+  tokenize(T, Stack, Len * 10 + (H - 48), blob_num);
+tokenize(<<":", T/binary>>, Stack, Len, blob_num) ->
   Tuple = {Len, []},
-  parse(T, Stack, Tuple, blob);
-parse(<<H, T/binary>>, Stack, {Len, Acc}, blob) when is_integer(Len) andalso Len > 0 ->
+  tokenize(T, Stack, Tuple, blob);
+tokenize(<<H, T/binary>>, Stack, {Len, Acc}, blob) when is_integer(Len) andalso Len > 0 ->
   Tuple = {Len - 1, [H | Acc]},
-  parse(T, Stack, Tuple, blob);
-parse(<<"'", T/binary>>, Stack, {0, Acc}, blob) ->
+  tokenize(T, Stack, Tuple, blob);
+tokenize(<<"'", T/binary>>, Stack, {0, Acc}, blob) ->
   Blob = erlang:list_to_binary(lists:reverse(Acc)),
-  parse(T, [Blob | Stack], nil, normal);
+  tokenize(T, [Blob | Stack], nil, normal);
 
 %% parse word
-parse(<<H, _/binary>> = Data, Stack, nil, normal) when ?is_alphabet(H) ->
-  parse(Data, Stack, [], word);
-parse(<<H, T/binary>>, Stack, Acc, word) when ?is_alphabet(H) ->
-  parse(T, Stack, [H | Acc], word);
-parse(Data, Stack, Acc, word) ->
+tokenize(<<H, _/binary>> = Data, Stack, nil, normal) when ?is_alphabet(H) ->
+  tokenize(Data, Stack, [], word);
+tokenize(<<H, T/binary>>, Stack, Acc, word) when ?is_alphabet(H) ->
+  tokenize(T, Stack, [H | Acc], word);
+tokenize(Data, Stack, Acc, word) ->
   Word = {word, lists:reverse(Acc)},
-  parse(Data, [Word | Stack], nil, normal);
+  tokenize(Data, [Word | Stack], nil, normal);
 
 %% parse number
-parse(<<H, T/binary>>, Stack, nil, normal) when ?is_digit(H) ->
-  parse(T, Stack, H - 48, number);
-parse(<<H, T/binary>>, Stack, Acc, number) when ?is_digit(H) ->
-  parse(T, Stack, Acc * 10 + (H - 48), number);
+tokenize(<<H, T/binary>>, Stack, nil, normal) when ?is_digit(H) ->
+  tokenize(T, Stack, H - 48, number);
+tokenize(<<H, T/binary>>, Stack, Acc, number) when ?is_digit(H) ->
+  tokenize(T, Stack, Acc * 10 + (H - 48), number);
 %%parse(<<H, _/binary>> = Data, Stack, Acc, number) when not (?is_digit(H)) ->
-parse(Data, Stack, Acc, number) ->
+tokenize(Data, Stack, Acc, number) ->
   io:format("finishing number, value=~w~n", [Acc]),
-  parse(Data, [Acc | Stack], nil, normal);
+  tokenize(Data, [Acc | Stack], nil, normal);
 
 %% parse floating number
 
 %% finished parsing state
-parse(<<>>, Stack, nil, normal) ->
+tokenize(<<>>, Stack, nil, normal) ->
   {ok, Stack};
 
 %% undefined parsing state
-parse(Data, Stack, Acc, Mode) ->
+tokenize(Data, Stack, Acc, Mode) ->
   State = #{
     data => Data,
     stack => Stack,
@@ -109,10 +111,22 @@ parse(Data, Stack, Acc, Mode) ->
   {error, State}.
 
 
-%%%%%%%%%%%%
-% from_ast %
-%%%%%%%%%%%%
-from_ast([{word, _} | _] = Ast) ->
-  from_ast(lists:reverse(Ast));
-from_ast([A | T]) ->
-  throw(error(implementing)).
+%%%%%%%%%%
+% parser %
+%%%%%%%%%%
+parse(Stack) ->
+  parse(Stack, nil, normal).
+
+parse([{word, Name} | T0], nil, normal) ->
+  {Children, [mark, T]} = lists:splitwith(fun(X) -> X =/= mark end, T0),
+  Node = #node{name = Name, children = Children},
+  parse([Node | T], nil, normal);
+
+parse(Stack, Acc, Mode) ->
+  State = #{
+    stack => Stack,
+    acc => Acc,
+    mode => Mode
+  },
+  io:format("undefined transform state ~~>~p~n", [State]),
+  {error, State}.
